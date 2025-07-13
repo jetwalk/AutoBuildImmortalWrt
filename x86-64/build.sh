21e92253e82ec273903b8fc4f4ef7ff250c3096b
jetwalk <69178819+jetwalk@users.noreply.github.com>
2025-07-13 10:20:44 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -33,7 +33,6 @@ PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-opkg-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
-PACKAGES="$PACKAGES luci-app-openclash"
 # WireGuard
 PACKAGES="$PACKAGES wireguard-tools"
 PACKAGES="$PACKAGES kmod-wireguard"
